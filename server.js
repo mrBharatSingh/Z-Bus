a8a3c78622651da605627c64016784fd8872fc8c
Bharat Singh Rathore <mrbharatsingh4@gmail.com>
2022-07-27 17:07:54 +0530

    heroku setting

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express=require("express")
+const env=require("dotenv")
 
 path=require("path")
 
@@ -8,7 +9,8 @@ const app=express()
 app.use(express.static(__dirname+'./dist/zbus'))
 
 app.get("/*",(req,res)=>{
-    res.sendFile(__dirname+'dist/zbus/index.html')
+    // res.sendFile(__dirname+'dist/zbus/index.html')
+    res.send({"peeka bhoo"})
 })
 
 app.listen(process.env.PORT|| 4200)
