ae0457d61a29ee5d8af1a3b4bbb203d740cb4c2c
Bharat Singh Rathore <mrbharatsingh4@gmail.com>
2022-07-28 09:22:55 +0530

    heroku setting done

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express=require("express")
 const env=require("dotenv")
 
-path=require("path")
+const path=require("path")
 
 
 const app=express()
 
-app.use(express.static(__dirname+'./dist/zbus'))
+app.use(express.static(__dirname + '/dist/zbus'))
 
 app.get("/*",(req,res)=>{
-    res.sendFile(__dirname+'/dist/zbus/index.html')
+    res.sendFile(__dirname + '/dist/zbus/index.html')
     // res.send({"res":"peeka bhoo"})
 })
 
